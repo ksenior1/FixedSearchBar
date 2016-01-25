30988d15969e70b5e09bba6650b9e440a7a8911d
Kemar Senior <kemarsenior@kemars-macbook-pro.local>
2016-01-25 11:43:07 -0500

    Initial Commit

---
+++ FixedSearchBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FixedSearchBar
+//
+//  Created by Kemar Senior on 1/25/16.
+//  Copyright © 2016 Kemar Senior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
